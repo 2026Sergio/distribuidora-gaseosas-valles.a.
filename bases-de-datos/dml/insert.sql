0a999858c695bdacba06da67fe906471bc0a06d0
2026sergio <mirandasegio550@gmail.com>
2026-08-12 21:13:11 -0600

    feat(dml): insertar datos de encargados

--- bases-de-datos/dml/insert.sql
+++ bases-de-datos/dml/insert.sql
@@ -8,3 +8,10 @@ INSERT INTO categorias (id_categoria, nombre_categoria) VALUES
 ('CAT-03', 'Jugos y Néctares'),
 ('CAT-04', 'Bebidas Energizantes'),
 ('CAT-05', 'Isotónicas y Deportivas');
+
+-- Encargados
+INSERT INTO encargados (id_encargado, nombre_encargado, telefono_encargado) VALUES
+('ENC-01', 'Mateo Morales', '+502 5910 1122'),
+('ENC-02', 'Sofia Méndez', '+502 4890 3344'),
+('ENC-03', 'Alejandro Estrada', '+502 3120 5566'),
+('ENC-04', 'María Fernanda López', '+502 5544 7788');
