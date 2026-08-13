641bd125e276ccc88b60cb28cbb3baed39f7e1ad
2026sergio <mirandasegio550@gmail.com>
2026-08-12 21:13:46 -0600

    feat(dml): insertar datos de almacenamiento

--- bases-de-datos/dml/insert.sql
+++ bases-de-datos/dml/insert.sql
@@ -15,3 +15,11 @@ INSERT INTO encargados (id_encargado, nombre_encargado, telefono_encargado) VALU
 ('ENC-02', 'Sofia Méndez', '+502 4890 3344'),
 ('ENC-03', 'Alejandro Estrada', '+502 3120 5566'),
 ('ENC-04', 'María Fernanda López', '+502 5544 7788');
+
+
+-- Almacenamientos
+INSERT INTO almacenamientos (id_almacenamiento, capacidad_cajas) VALUES
+('ALM-01', 25000),
+('ALM-02', 40000),
+('ALM-03', 18000),
+('ALM-04', 30000);
