cc535a886ea7f4a0512e529ae1e6a772bc4253bb
2026sergio <mirandasegio550@gmail.com>
2026-08-12 21:12:37 -0600

    feat(dml): insertar datos de categoria

---
+++ bases-de-datos/dml/insert.sql
@@ -0,0 +1,10 @@
+USE distribuidora_bebidas;
+
+
+-- Categorías
+INSERT INTO categorias (id_categoria, nombre_categoria) VALUES
+('CAT-01', 'Gaseosas Tradicionales'),
+('CAT-02', 'Aguas Purificadas'),
+('CAT-03', 'Jugos y Néctares'),
+('CAT-04', 'Bebidas Energizantes'),
+('CAT-05', 'Isotónicas y Deportivas');
